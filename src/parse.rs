70b9bc4956b47fabdeb865040e05d95d2034324e
Daniel Rivas <daniel@silicoai.com>
2021-11-24 09:27:40 +0000

    Parse HSL units properly (except for grads)

--- src/parse.rs
+++ src/parse.rs
@@ -2,31 +2,79 @@ use crate::colors::HslColor;
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{space0, space1},
-    combinator::map,
+    character::complete::{one_of, space0, space1},
+    combinator::{map, opt},
     number::complete::float,
-    sequence::{delimited, tuple},
+    sequence::{delimited, terminated, tuple},
     IResult,
 };
 
 fn comma_or_spaces(input: &str) -> IResult<&str, ()> {
-    map(alt((space1, delimited(space0, tag(","), space0))), |_| ())(input)
+    map(alt((delimited(space0, tag(","), space0), space1)), |_| ())(input)
 }
-fn float_list_of_three(input: &str) -> IResult<&str, (f32, f32, f32)> {
+
+#[derive(Debug, PartialEq)]
+enum Angle {
+    Degrees(f32),
+    Radians(f32),
+    Gradians(f32),
+    Turns(f32),
+}
+
+impl Angle {
+    fn to_degrees(self) -> f32 {
+        match self {
+            Self::Degrees(deg) => deg,
+            Self::Radians(rad) => rad.to_degrees(),
+            Self::Gradians(grad) => unimplemented!(),
+            Self::Turns(turns) => turns * 360.0,
+        }
+    }
+}
+
+fn angle(input: &str) -> IResult<&str, Angle> {
+    let parser = tuple((
+        float,
+        opt(alt((tag("deg"), tag("rad"), tag("grad"), tag("turn")))),
+    ));
+
+    map(parser, |(val, unit)| match unit {
+        None | Some("deg") => Angle::Degrees(val),
+        Some("rad") => Angle::Radians(val),
+        Some("grad") => Angle::Gradians(val),
+        Some("turn") => Angle::Turns(val),
+        _ => unreachable!(),
+    })(input)
+}
+
+fn percentage(input: &str) -> IResult<&str, f32> {
+    terminated(float, opt(tag("%")))(input)
+}
+
+fn hsl_triple(input: &str) -> IResult<&str, (Angle, f32, f32)> {
     let parser = delimited(
         space0,
-        tuple((float, comma_or_spaces, float, comma_or_spaces, float)),
+        tuple((
+            angle,
+            comma_or_spaces,
+            percentage,
+            comma_or_spaces,
+            percentage,
+        )),
         space0,
     );
 
-    map(parser, |(f1, _, f2, _, f3)| (f1, f2, f3))(input)
+    map(parser, |(angle, _, sat, _, lum)| (angle, sat, lum))(input)
 }
 
 fn hsl_color(input: &str) -> IResult<&str, HslColor> {
     let (input, (hue, saturation, luminosity)) =
-        delimited(tag("hsl("), float_list_of_three, tag(")"))(input)?;
+        delimited(tag("hsl("), hsl_triple, tag(")"))(input)?;
 
-    Ok((input, HslColor::new(hue, saturation, luminosity)))
+    Ok((
+        input,
+        HslColor::new(hue.to_degrees(), saturation, luminosity),
+    ))
 }
 
 #[cfg(test)]
@@ -35,21 +83,34 @@ mod test {
 
     #[test]
     fn parse_comma_or_spaces() {
-        comma_or_spaces("    aoe").unwrap();
-        comma_or_spaces(",  ").unwrap();
-        comma_or_spaces(" , ").unwrap();
-        comma_or_spaces("  ,").unwrap();
-        comma_or_spaces(", ").unwrap();
-        comma_or_spaces("   ").unwrap(); // Tab
+        assert_eq!(comma_or_spaces("    aoe").unwrap().0, "aoe");
+        assert_eq!(comma_or_spaces(",  ").unwrap().0, "");
+        assert_eq!(comma_or_spaces(" , ").unwrap().0, "");
+        assert_eq!(comma_or_spaces("  ,").unwrap().0, "");
+        assert_eq!(comma_or_spaces(", ").unwrap().0, "");
+        assert_eq!(comma_or_spaces("   ").unwrap().0, "");
+        assert_eq!(comma_or_spaces("    ").unwrap().0, "");
 
         assert!(comma_or_spaces("l ").is_err());
     }
 
     #[test]
     fn parse_float_list() {
-        let (rest, output) = float_list_of_three("32,11.22,04oeeooe").unwrap();
-        assert_eq!(output, (32.0, 11.22, 4.0));
-        assert_eq!(rest, "oeeooe")
+        let (rest, output) = hsl_triple("32,11.22,04oeeooe").unwrap();
+        assert_eq!(output, (Angle::Degrees(32.0), 11.22, 4.0));
+        assert_eq!(rest, "oeeooe");
+
+        let (rest, output) = hsl_triple("32deg, 11.22,04oeeooe").unwrap();
+        assert_eq!(output, (Angle::Degrees(32.0), 11.22, 4.0));
+        assert_eq!(rest, "oeeooe");
+
+        let (rest, output) = hsl_triple("32deg   , 11.22,04oeeooe").unwrap();
+        assert_eq!(output, (Angle::Degrees(32.0), 11.22, 4.0));
+        assert_eq!(rest, "oeeooe");
+
+        let (rest, output) = hsl_triple("360rad 12% 34").unwrap();
+        assert_eq!(output, (Angle::Radians(360.0), 12.0, 34.0));
+        assert_eq!(rest, "");
     }
 
     #[test]
@@ -63,5 +124,13 @@ mod test {
 
         let (_, color) = hsl_color("hsl(  212 12  24.2)").unwrap();
         assert_eq!(color, HslColor::new(212.0, 12.0, 24.2));
+
+        let (_, color) = hsl_color("hsl(2turn 24.3, 4%)").unwrap();
+        // Clamped at max 360
+        assert_eq!(color, HslColor::new(360.0, 24.3, 4.0));
+
+        let (_, color) = hsl_color("hsl(2turn -24.3, 101%)").unwrap();
+        // Clamped at max 360, max 100, max 100, min 0
+        assert_eq!(color, HslColor::new(360.0, 0.0, 100.0));
     }
 }
